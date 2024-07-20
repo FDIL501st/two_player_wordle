e8358d3867e87fb5c990078ba5df5ddd4b4a032d
FDIL <fdil.fadilh1@gmail.com>
2024-07-20 03:33:52 -0600

    First defination of custom server error.
    To be used in returns on queries and mutations for the Error part of the Result.
    Subject to change in future.
    
    Signed by: FDIL<fdil.fadilh1@gmail.com>

--- graphql_server/src/errors.rs
+++ graphql_server/src/errors.rs
@@ -1,2 +1,32 @@
 use std::fmt;
 
+use juniper::{graphql_value, Value};
+
+#[derive(Debug)]
+pub struct GraphalError {
+    pub message: String,
+    pub code: u8,
+    pub code_message: String
+}
+
+impl GraphalError {
+    pub fn new(message: String, code: u8, code_message: String) -> GraphalError {
+        GraphalError {
+            message,
+            code,
+            code_message
+        }
+    }
+}
+
+impl fmt::Display for GraphalError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let error_json: Value<String> = graphql_value!({
+            "error": self.message.clone(), 
+            "code": self.code.to_string(), 
+            "message": self.code_message.clone()
+        });
+
+        write!(f, "{:#?}", error_json)
+    }
+}
