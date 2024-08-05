f1c0d87b2cb54c67c169ef97ff1c758ec50266c5
FDIL <fdil.fadilh1@gmail.com>
2024-08-05 15:52:36 -0600

    Initialize client next.js project
    Signed-off-by: FDIL <fdil.fadilh1@gmail.com>

---
+++ client/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div>
+        2 player wordle client is online
+      </div>
+    </main>
+  );
+}
