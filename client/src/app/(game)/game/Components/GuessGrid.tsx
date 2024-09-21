3dfaf2521d22d3469dea63fe58b703c5f98afbb7
FDIL <fdil.fadilh1@gmail.com>
2024-09-21 14:58:07 -0600

    Add GuessRow and GuessBox
    Added the children components of GuessGrid. Not fully defined yet.
    
    Signed-off-by: FDIL <fdil.fadilh1@gmail.com>

--- client/src/app/(game)/game/Components/GuessGrid.tsx
+++ client/src/app/(game)/game/Components/GuessGrid.tsx
@@ -1,22 +1,26 @@
 'use client'
 
-import {useState} from "react";
+import {useEffect, useState} from "react";
+import {useGuess} from "@/game/Contexts/GuessProvider";
 
 const GuessGrid = () => {
   const [currentRow, setCurrentRow] = useState<number>(0)
   // the total number of guesses/rows
-  const numGuesses: number = 5
+  const numGuesses: number = 6
   const rowNumbers: number[] = Array.from({ length: numGuesses }, (_, i) => i);
   // row numbers/indexes start from 0
 
   // number of letters word being guessed are
   const wordSize: number = 5
 
+  // the guess
+  const guess = useGuess()
+
   return (
     <div>
       {rowNumbers.map((rowNumber) => (
         <div key={rowNumber}>
-          <GuessRow isActive={currentRow === rowNumber} wordSize={wordSize}/>
+          <GuessRow isActive={currentRow === rowNumber} wordSize={wordSize} guess={guess} />
         </div>
       ))}
     </div>
@@ -34,10 +38,43 @@ export default GuessGrid
 
 interface GuessRowProps {
   isActive: boolean,
-  wordSize: number
+  wordSize: number,
+  guess: string
 }
-const GuessRow = ({isActive, wordSize}: GuessRowProps) => {
+const GuessRow = ({isActive, wordSize, guess}: GuessRowProps) => {
+  // the word/guess being displayed by this row
+  const [word, setWord] = useState<string>("")
+
+  useEffect(() => {
+    // do nothing if current row is not active
+    if (!isActive) return
+
+    // update word everytime guess is updated
+    setWord(guess)
+
+  }, [isActive]);
+
+  const letterNumbers: number[] = Array.from({ length: wordSize }, (_, i) => i)
+
   return (
-    <></>
+    <div>
+      {letterNumbers.map((letterNumber) => {
+        // display no text for boxes without a letter yet
+        const letter: string = letterNumber < word.length ? word[letterNumber] : ""
+
+        return (
+          <GuessBox letter={letter} />
+        )
+      })}
+    </div>
+  )
+}
+
+const GuessBox = ({letter}: {letter: string}) => {
+  // need to update box colour somehow before updating currentRow
+  return(
+    <div>
+      {letter}
+    </div>
   )
 }
