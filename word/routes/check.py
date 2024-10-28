677b7e547c87874e933495624cfecd1f961515b6
FDIL <fdil.fadilh1@gmail.com>
2024-10-28 10:11:53 -0600

    Created the word server.
    Features include getting a random five-letter word and checking if a five-letter word exists in the db.
    
    Updated compose.yaml so word-server is part of the app.
    
    Signed-off-by: FDIL <fdil.fadilh1@gmail.com>

---
+++ word/routes/check.py
@@ -0,0 +1,20 @@
+"""
+Routes that deals with checks.
+
+For example, checking if a word is valid.
+Might also include comparison between two words.
+"""
+
+from fastapi import APIRouter
+from db.read import get_random_five_letter_word
+
+router = APIRouter(
+    tags=["Word"],
+    prefix="/word"
+)
+
+
+@router.get('/five_letter', response_model=str)
+async def get_five_letter_word() -> str:
+    """Returns a random five letter word."""
+    return await get_random_five_letter_word()
