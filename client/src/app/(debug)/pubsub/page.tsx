5dd48830be8485495ec7e7fe0ab6d9f001c39cc2
FDIL <fdil.fadilh1@gmail.com>
2026-04-13 21:55:35 -0600

    Improved message broker demo
    Got rid of java middleman as not needed for this demo.
    
    Instead, directly make connections and pub/sub to rabbitmq.
    Also improved the amount of websocket connections being made, now actully only making 2, one for pub and other for sub.

--- client/src/app/(debug)/pubsub/page.tsx
+++ client/src/app/(debug)/pubsub/page.tsx
@@ -1,63 +1,100 @@
 'use client';
 
-import { Client } from "@stomp/stompjs";
+import { Client, IMessage } from "@stomp/stompjs";
 import { BSON } from "bson";
-import {useEffect, useState} from "react";
+import {useEffect, useState, useRef} from "react";
 
 const PubSub = () => {
+    const clientPubRef = useRef<Client | null>(null);
     const [inputMessage, setInputMessage] = useState("");
     const [message, setMessage] = useState({});
 
 
-    const clientPub = new Client({
-        brokerURL: "ws://localhost:8080/mb-ws",
-            onConnect: () => {
-                // encode your data as BSON bytes
-                const bsonBytes = BSON.serialize({
-                  text: inputMessage,
-                  user: "self",
-                });
+    function sendMessage() {
+        const clientPub = clientPubRef.current;
+        if (!clientPub) {
+            console.error("Publisher client is not initialized.");
+            return;
+        }
 
-                clientPub.publish({
-                  destination: "/app/chat", // goes to Spring first
-                  binaryBody: bsonBytes,
-                  headers: { "content-type": "application/octet-stream" },
-                });
-            },
-    });
+        // encode your data as BSON bytes
+        const bsonBytes = BSON.serialize({
+            text: inputMessage,
+            user: "self",
+        });
 
-    const clientSub = new Client({
-        brokerURL: "ws://localhost:8080/mb-ws",
-        onConnect: () => {
-            clientSub.subscribe("/topic/chat", (message) => {
-            // decode the BSON bytes back
-            const data = BSON.deserialize(message.binaryBody);
-            console.log(data);
-            setMessage(data);
-            clientPub.deactivate()      // deactivate publisher after receiving the message
-            });
-        },
-    });
+        clientPub.publish({
+            destination: "/topic/chat", // goes to Spring first
+            binaryBody: bsonBytes,
+            headers: { "content-type": "application/octet-stream" },
+        });
+    }
+
+    function handleMessage(message: IMessage) {
+        // decode the BSON bytes back
+        // expect data to be binary and have content-type header set to application/octet-stream
+        const data = BSON.deserialize(message.binaryBody);
+        setMessage(data);
+    }
+
+
 
 
     useEffect(() => {
+        const clientPub = new Client({
+            brokerURL: "ws://localhost:15674/ws",
+            connectHeaders: {
+                login: "guest",
+                passcode: "guest",
+            },
+            onConnect: () => {
+                console.log("Publisher connected");
+            },
+            debug: (str) => {
+                console.log("STOMP Pub Debug:", str);
+            },
+            onWebSocketError: (event) => {
+                console.error("WebSocket Pub error:", event);
+            },
+            onStompError: (frame) => {
+                console.error("STOMP Pub error:", frame);
+            },
+        })
+        clientPubRef.current = clientPub;
+        clientPub.activate();
+        
+        const clientSub = new Client({
+            brokerURL: "ws://localhost:15674/ws",
+            connectHeaders: {
+                login: "guest",
+                passcode: "guest",
+            },
+            onConnect: () => {
+                clientSub.subscribe("/topic/chat", handleMessage);
+            },
+            debug: (str) => { 
+                console.log("STOMP Sub Debug:", str) 
+            },
+            onWebSocketError: (event) => {
+                console.error("WebSocket Sub error:", event);
+            },
+            onStompError: (frame) => {
+                console.error("STOMP Sub error:", frame);
+            },
+        });
         clientSub.activate()
 
         return () => {
             clientSub.deactivate()
+            clientPub.deactivate()
         }
     }, []);
-
-
-    async function publishMessage() {
-        clientPub.activate();
-    }
 
     return (
       <div>
         <h1>Pub/Sub Test</h1>
         <input type="text" value={inputMessage} onChange={(e) => setInputMessage(e.target.value)} />
-        <button onClick={publishMessage}>Publish</button>
+        <button onClick={sendMessage}>Publish</button>
 
         <h3>{JSON.stringify(message)}</h3>
       </div>
